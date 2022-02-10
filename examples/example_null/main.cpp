1d9c1ea630428ac6101a33c29d2598388a2985bc
Carlo Wood <carlo@alinoe.com>
2022-02-10 12:43:47 +0100

    Finished this MCVE.
    It turns out that imgui handles a button release followed by a mouse
    move outside of the window correctly. During my tests I probably
    moved my mouse outside of the button before releasing it.
    
    I haven't tested what happens when include a loss of focus instead
    however -- it is still possible that if you release the mouse
    on the button, then move to another window and click the mouse
    button there, causing the previous window to lose focus, all before
    the next frame starts, that this button release is lost.

--- examples/example_null/main.cpp
+++ examples/example_null/main.cpp
@@ -12,7 +12,8 @@ int main()
     int tex_w, tex_h;
     io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
 
-    for (int n = 0; n < 3; n++)
+    bool last_frame = false;
+    for (int n = 0; n < 20; n++)
     {
         io.DisplaySize = ImVec2(150, 50);
         io.DeltaTime = 1.0f / 60.0f;
@@ -20,26 +21,43 @@ int main()
         switch (n)
         {
           case 0:
+//            io.AddFocusEvent(true);
             break;
           case 1:
             io.AddMousePosEvent(75, 25);
             io.AddMouseButtonEvent(0, true);
             break;
           case 2:
-//            io.AddMousePosEvent(-FLT_MAX, -FLT_MAX);
+//            break;
+//          case 3:
+            io.AddMousePosEvent(-FLT_MAX, -FLT_MAX);
             io.AddMousePosEvent(76, 24);
             io.AddMouseButtonEvent(0, false);
-//            io.AddMousePosEvent(-FLT_MAX, -FLT_MAX);
+            io.AddMousePosEvent(-FLT_MAX, -FLT_MAX);
+            break;
+          default:
+            last_frame = true;
             break;
         }
+        if (last_frame)
+          break;
 
         printf("NewFrame() %d\n", n);
         ImGui::NewFrame();
 
+        ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+        ImGui::SetNextWindowSize(io.DisplaySize);
+        ImGui::Begin("SingleButton", nullptr,
+            ImGuiWindowFlags_NoDecoration |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoSavedSettings);
+
         if (ImGui::Button("Trigger Event", ImVec2(134, 134)))
-          printf("Button clicked!\n");
+          printf(">>>>>>>>>>> Button clicked!\n");
         else
-          printf("Button not clicked!\n");
+          printf(">>>>>>>>>>> Button not clicked!\n");
+
+        ImGui::End();
 
         ImGui::EndFrame();
         ImGui::Render();
